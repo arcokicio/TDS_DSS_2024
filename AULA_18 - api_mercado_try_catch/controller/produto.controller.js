f0dfc6aa699719fe6b168e0d31e361f798fd929d
arcokicio <151792143+arcokicio@users.noreply.github.com>
2024-10-30 19:04:14 -0300

    Add files via upload

--- AULA_18 - api_mercado_try_catch/controller/produto.controller.js
+++ AULA_18 - api_mercado_try_catch/controller/produto.controller.js
@@ -1,96 +1,79 @@
 const conn = require("../mysql-connection");
 
 module.exports = {
-    cadastro:  async (req, res) => {
-        const { id, nome, preco } = req.body;
-        try{
-            const data = await conn.raw(`INSERT INTO CLIENTE(nome, preco) VALUES ('${nome}', '${preco}'`),
-            
+    cadastro: async (req, res) => {
+        const { nome, preco } = req.body;
 
-            if (!nome | !id | !preco) {
-                return res.status(309).send({msg: "O campo é obrigatorio"}) 
-
-            
-
-        }catch(error)({
-            res.status(200).send({msg: "Erro ao cadastrar cliente"})
-        })
-
-    }
-        
-    
-                   
-        
-
-
-
-
-
-
-        var comando = ``;
-
-        if (!telefone) {
-            comando = `INSERT INTO CLIENTE(nome, 
-            telefone) VALUES('${nome}',null)`
-        } else {
-            comando = `INSERT INTO CLIENTE(nome, 
-            telefone) VALUES('${nome}','${telefone}')`
+        if (!preco) {
+            return res.status(309).send({ msg: "É obrigatorio enviar o campo preco!" });
+        } else if (typeof preco !== "number") {
+            return res.status(309).send({ msg: "O campo preco precisa ser do tipo numerico!" });
         }
 
-        conn.raw(comando)
-            .then((data) => {
-                res.status(200).send({ msg: "Cliente cadastrado com sucesso!" });
-            })
-            .catch((error) => {
-                res.status(500).send("Erro ao cadastrar um cliente!");
-            });
+        try {
+            const data = await conn.raw(`INSERT INTO PRODUTO(nome, preco) VALUES ('${nome}', '${preco}'`);
+            return res.status(200).send(data[0]);
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ msg: "Erro ao cadastrar produto" })
+        }
 
     },
 
+    consultar: async (req, res) => {
 
-    consultar: async (req,res)=>{
-       
-        try{
+        try {
             const data = await conn.raw("SELECT * FROM PRODUTO");
-        }catch(error){
+        } catch (error) {
             console.log(error);
-            return res.status(500).send({msg: "erro ao consultar produtos"});
-                }
-       
+            return res.status(500).send({ msg: "erro ao consultar produtos" });
+        }
+
         console.log(data);
         res.send(data[0]);
 
     },
-    
-    atualizar: async (req, res)=>{
-        const {nome, preco} = req.body;
-        try{
+
+    atualizar: async (req, res) => {
+        const { nome, preco } = req.body;
+        try {
             const data = await conn.raw(`UPDATE CLIENTE SET preco='${preco}',
             WHERE id =  ${nome}`)
             console.log(data);
             res.status(200).send({ msg: "Preco atualizado com sucesso!" })
-        }catch{
+        } catch {
             console.log(error);
-            res.status(500).send({ msg: "Erro ao atualizar o preço!" });          
+            res.status(500).send({ msg: "Erro ao atualizar o preço!" });
 
-        }},
+        }
+    },
 
-    deletar: async (req, res)=>{
-        const {id} = req.params;
-        try{
+    deletar: async (req, res) => {
+        const { id } = req.params;
+        try {
             const result = await conn.raw(`DELETE FROM CLIENTE WHERE ID = ?`, [id]);
 
-            if (result.affectedRows === 0){
-                return res.status(404).send({msg: "cliente não encontrado"});
+            if (result.affectedRows === 0) {
+                return res.status(404).send({ msg: "cliente não encontrado" });
             }
-            return res.status(404).send({ msg: "Cliente não encontrado"});
+            return res.status(404).send({ msg: "Cliente não encontrado" });
 
-        }catch(error){
+        } catch (error) {
             console.log(error);
-            return res.status(500).send({msg:"Erro ao deletar o produto"});        
+            return res.status(500).send({ msg: "Erro ao deletar o produto" });
         }
-        
-        
+
+
+    },
+    buscaPorId: async (req, res) => {
+        const { id } = req.params;
+
+        // const data = await conn.raw(`SELECT * FROM PRODUTO WHERE id = ${id}`);
+        const data = await conn.select("nome", "preco")
+            .from("produto")
+            .where({ id });
+
+        return res.status(200).send(data);
     }
 
 }
