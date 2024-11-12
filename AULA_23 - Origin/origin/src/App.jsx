b0cb4f4b4fa6d24e2140e987a1e286198d1b7613
arcokicio <151792143+arcokicio@users.noreply.github.com>
2024-11-11 21:45:52 -0300

    Add files via upload

---
+++ AULA_23 - Origin/origin/src/App.jsx
@@ -0,0 +1,15 @@
+import TheReal from "./TheReal"
+
+
+function App() {
+  
+  return (
+    <>
+    <h1> Ola mundo!</h1>
+    <TheReal/>
+    </>
+  )
+}
+
+export default App
+
---
+++ AULA_23 - Origin/origin/src/TheReal.jsx
@@ -0,0 +1,8 @@
+import "./TheReal.css"
+
+function TheReal(){
+    return (
+        <h2> Componente </h2>
+    )
+}
+export default TheReal
